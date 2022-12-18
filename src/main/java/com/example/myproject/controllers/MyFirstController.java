db99cecc9f8d21a862db720e8cba541f93aa0bff
Света <svetik24082@mail.ru>
2022-12-19 00:32:50 +0500

    Введение в Maven и сборку проектов

---
+++ src/main/java/com/example/myproject/controllers/MyFirstController.java
@@ -0,0 +1,20 @@
+package com.example.myproject.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MyFirstController {
+    @GetMapping
+    public String processTheRequest() {
+        return " Приложение запущено ";
+    }
+
+    @GetMapping("/path/to/info")
+    public String info() {
+        return   " Головина Светлана, "  +  "Мой первый проект,"  + "19.12.2022г.," +
+                " Мой первый опыт создания веб-приложения ";
+    }
+}
